711ff38a44775094cf83bc186567c3dc7ef8897d
Menelaos Kotsollaris <menelaos@mkotsollaris.com>
2017-07-03 18:31:15 -0300

    Initial Commit

---
+++ Squirrel/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Squirrel
+//
+//  Created by mkotsollaris on 2017-07-03.
+//  Copyright © 2017 mkotsollaris. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Squirrel/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Squirrel
+//
+//  Created by mkotsollaris on 2017-07-03.
+//  Copyright © 2017 mkotsollaris. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
